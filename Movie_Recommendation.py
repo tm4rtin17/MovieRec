2df4c3692ba4aad182cb7eae097aca8e0b024a24
Thomas Martin <87400753+tm4rtin17@users.noreply.github.com>
2025-01-09 13:09:00 -0800

    Update Movie_Recommendation.py
    Fixed formatting. Added page breaks and removed blank print statements. Added "Here is a list of ___ movies: " to each genre.

--- Movie_Recommendation.py
+++ Movie_Recommendation.py
@@ -15,30 +15,30 @@ movie_matrix = [['Friday the 13th', 'Horror', 8, 'A group of teenage camp counse
 
 #Define user input function
 def select_genre():
-    print('[1] Horror, [2] Comedy, [3] Action')
-    user_genre = pyip.inputNum("What genre of movie were you thinking about watching? Enter the corresponding number", min=1, max=3)
+    print('\f[1] Horror, [2] Comedy, [3] Action')
+    user_genre = pyip.inputNum("What genre of movie were you thinking about watching? Enter the corresponding number\n", min=1, max=3)
     return user_genre
 
 #Sort through movies of the genre selected in a list format
 def sort_genre(user_genre):
-    
     count = 1       #Counter to define numbered list in print statement
-    print('')       #Prints blank space for clean formatting
 
     if user_genre == 1: 
-        print("Here is a list of horror movies: ")
+        print("\fHere is a list of horror movies: \n")
         for movie in movie_matrix:
             if movie[1] == 'Horror':
                 print(f'{count}.{movie[0]} ({movie[2]}/10 stars)')
                 movie_list.append(movie)
                 count += 1
     elif user_genre == 2:
+        print("\fHere is a list of comedy movies: \n")
         for movie in movie_matrix:
             if movie[1] == 'Comedy':
                 print(f'{count}.{movie[0]} ({movie[2]}/10 stars)')
                 movie_list.append(movie)
                 count += 1
     elif user_genre == 3:
+        print("\fHere is a list of horror movies: \n")
         for movie in movie_matrix:
             if movie[1] == 'Action':
                 print(f'{count}.{movie[0]} ({movie[2]}/10 stars)')
@@ -47,10 +47,8 @@ def sort_genre(user_genre):
 
 #Allows the user to select a movie and prints its description
 def select_movie():
-    print('')       #Blank print statement for formatting only
-    mov = (pyip.inputNum("Enter the corresponding number of the movie you are interested in...", min=0, max=3)) - 1   #User input to select a movie
-    print('')                           #Prints blank space for formatting cleanliness
-    print(f'{movie_list[mov][0]}: ')    #Prints the movie name
+    mov = (pyip.inputNum("\fEnter the corresponding number of the movie you are interested in...\n", min=0, max=3)) - 1   #User input to select a movie
+    print(f'\f{movie_list[mov][0]}: ')    #Prints the movie name
     print(f'{movie_list[mov][3]}')      #Prints the movie description
     movie_list.clear()                  #Clears out the list so the user can search for more movies
 
@@ -62,15 +60,13 @@ sort_genre(user_genre)
 select_movie()
 
 while True:
-    rerun = pyip.inputYesNo('Would you like to look for another movie? (y/n)')
+    rerun = pyip.inputYesNo('\fWould you like to look for another movie? (y/n)\n')
     if rerun is 'yes':
-        print('')       #Blank space for formatting only
-        
+
         #Functions rerun infinitely as desired
         user_genre = select_genre()
         sort_genre(user_genre)
         select_movie()
     else:
-        print('')       #Blank space for formatting only
-        print('Enjoy your movie!')
+        print('\fEnjoy your movie!')
         break
