86b2e99062411e743c8e86457059125e8903bd0f
tm4rtin17 <tm4rtin17@gmail.com>
2025-01-12 21:06:16 -0800

    - Removed infinite loop from 'main.py' and nested it within the `main()` function. - Created new logic inside infinite loop to allow the user to move back a page to see the list of movies of the selected genre. - Various clerical adjustments made to print statements. - Updated changelog in README.

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import pyinputplus as pyip
 from mov_data import *
 from mov_func import *
 
-#Initial run of the main function
-main()
-
 #Infinite loop to allow user to search an indefinite amount of times until ready to quit
 while True:
-    rerun = pyip.inputYesNo('\fWould you like to look for another movie? (y/n)\n')
-    if rerun is 'yes':
-        main()
-    else:
-        print('\fEnjoy your movie!') #no answer ends the program
-        break
+    main()
